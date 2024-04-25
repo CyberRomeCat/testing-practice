06e0c5c12fbe336a020450ad32b00993f8f84a2d
jenzie68 <jacecabalo@gmail.com>
2024-04-25 21:22:21 +0400

    test: calc function passed && refactor code

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function capitalize(word) {
-  let capitalize = word[0].toUpperCase() + word.slice(1);
-  return capitalize;
+function testSubject(test) {
+  if (typeof test == "string") {
+    let reversed = test.split("").reverse().join("");
+    let capitalize = reversed[0].toUpperCase() + reversed.slice(1);
+    return capitalize;
+  } else if (typeof test == "number") {
+    return test;
+  }
 }
 
-module.exports = capitalize;
+module.exports = testSubject;
--- script.test.js
+++ script.test.js
@@ -1,5 +1,30 @@
 const script = require("./script");
 
 test("Capitalize first letter", () => {
-  expect(script("love")).toBe("Love");
+  expect(script("love")).toBe("Evol");
+});
+
+test("reverseString", () => {
+  expect(script("love")).toBe("Evol");
+});
+
+test("calculator", () => {
+  const calc = {
+    add: () => {
+      return 2 + 2;
+    },
+    subtract: () => {
+      return 5 - 1;
+    },
+    multiply: () => {
+      return 3 * 2;
+    },
+    divide: () => {
+      return 2 / 2;
+    },
+  };
+  expect(script(calc.add())).toBe(4);
+  expect(script(calc.subtract())).toBe(4);
+  expect(script(calc.multiply())).toBe(6);
+  expect(script(calc.divide())).toBe(1);
 });
